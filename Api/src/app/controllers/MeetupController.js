f4d2cfa2fb03cd71ae3958d6a649d3da4556437d
Ryan Drumond <ryandrumond@outlook.com.br>
2019-09-04 23:52:27 -0300

    Implementando store de meetups

--- Api/src/app/controllers/MeetupController.js
+++ Api/src/app/controllers/MeetupController.js
@@ -1,10 +1,41 @@
+import * as Yup from 'Yup';
+import { isAfter, parseISO } from 'date-fns';
+
 import Meetup from '../models/Meetup';
 
-class Meetup {
+class MeetupController {
   async store(req, res) {
 
-    return res.json();
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      locale: Yup.string().required(),
+      banner_id: Yup.number().required()
+    });
+
+    if(!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: "Validation fails" });
+
+    const { title, description, date, locale, banner_id } = req.body;
+    const { userId } = req;
+
+    if(!userId)
+      return res.status(401).json({ error: "This method is not permitted" });
+
+    if(!isAfter(parseISO(date), new Date()))
+      return res.status(400).json({ error: "Past dates are not permitted" });
+    
+    const meetup = await Meetup.create({
+      title,
+      description,
+      locale,
+      date,
+      banner_id,
+      user_id: userId
+    });
+
+    return res.json(meetup);
   }
 }
 
-export default new Meetup();
+export default new MeetupController();
